36814f0ae9767579ddc2824d3cd56c6225e34eb8
yunusakin <yunusakin@gmail.com>
2025-02-09 16:05:02 +0300

    Initial commit

---
+++ src/main/java/com/yunusakin/credit/module/api/repository/domain/Customer.java
@@ -0,0 +1,6 @@
+package com.yunusakin.credit.module.api.repository.domain;
+
+import com.yunusakin.credit.module.common.domain.BaseEntity;
+
+public class Customer extends BaseEntity {
+}
---
+++ src/main/java/com/yunusakin/credit/module/common/domain/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.yunusakin.credit.module.common.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public abstract class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+    @Column(name = "deleted_date")
+    private LocalDateTime deletedDate;
+
+    @Column(name = "deletable")
+    private boolean deletable = true;
+}
